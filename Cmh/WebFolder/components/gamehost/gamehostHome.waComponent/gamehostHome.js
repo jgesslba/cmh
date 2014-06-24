d22df07eb12db5a3ea91acae9d91d682990ba242
jgesslba <joerg.gesslbauer@gmail.com>
2014-06-24 21:23:11 +0200

    Webfolder
    Add components folder.

---
+++ Cmh/WebFolder/components/gamehost/gamehostHome.waComponent/gamehostHome.js
@@ -0,0 +1,72 @@
+
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'gamehostHome';
+	// @endregion// @endlock
+
+	// @region Translation
+	var translateWidgets = function () {
+		// Get translated values
+		$comp.sourcesVar.objTranslation.richTextWelcome = translate($comp.name, "richTextWelcome");
+		
+		// Sync datasources
+		$comp.sources.objTranslation.sync();
+	};
+	
+	WAF.addListener("iconAustria", "click", function(event) {
+		// Translate component widgets on component load
+		language.getUserLanguageAsync({
+        	'onSuccess': function (result) {
+				translateWidgets(); // Call the translation function for this page
+        	},  
+        	'onError': function (error) {
+         		console.log(error);
+           	}
+        });
+  	});
+  	
+  	WAF.addListener("iconGreatBritain", "click", function(event) {
+		// Translate component widgets on component load
+		language.getUserLanguageAsync({
+        	'onSuccess': function (result) {
+				translateWidgets(); // Call the translation function for this page
+        	},  
+        	'onError': function (error) {
+         		console.log(error);
+           	}
+        });
+  	});
+ 	// @endregion
+ 	
+	this.load = function (data) {// @lock
+
+	// Translate component widgets on component load
+		language.getUserLanguageAsync({
+        	'onSuccess': function (result) {
+				translateWidgets(); // Call the translation function for this page
+        	},  
+        	'onError': function (error) {
+         		console.log(error);
+           	}
+        });
+	
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
