4e3a02309d4a8c45cfe07fcabb98a47dfbcecd8d
jgesslba <joerg.gesslbauer@gmail.com>
2014-06-25 20:47:07 +0200

    bootstraps
    Add systemdata file

--- Cmh/Configuration/01_Country.js
+++ Cmh/Configuration/01_Country.js
@@ -1,21 +1,21 @@
-// ##### DELETE ALL COUNTRIES #####
+//// ##### DELETE ALL COUNTRIES #####
 
-ds.Country.remove();
+//ds.Country.remove();
 
 
 // ##### CREATE DEFAULT COUNTRIES #####
 
-var newCountry = new ds.Country();
-newCountry.name = "Austria";
-newCountry.iso = "at";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
-newCountry.save();
+//var newCountry = new ds.Country();
+//newCountry.name = "Austria";
+//newCountry.iso = "at";
+//newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
+//newCountry.save();
 
-var newCountry = new ds.Country();
-newCountry.name = "United Kingdom";
-newCountry.iso = "de";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
-newCountry.save();
+//var newCountry = new ds.Country();
+//newCountry.name = "United Kingdom";
+//newCountry.iso = "de";
+//newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
+//newCountry.save();
 
 
 // ## SHOW ALL COUNTRIES ##
--- Cmh/bootstraps/loginlistener.js
+++ Cmh/bootstraps/loginlistener.js
@@ -2,3 +2,5 @@
 
 // Set directory login listener
 directory.setLoginListener('Login','Internal'); // login listener using password key
+
+
