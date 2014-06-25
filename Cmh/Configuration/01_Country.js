73940909040af5636c9ee9a7417229c6a43141c6
jgesslba <joerg.gesslbauer@gmail.com>
2014-06-25 20:05:58 +0200

    Model
    Set iso attribute in country class as unique.

--- Cmh/Configuration/01_Country.js
+++ Cmh/Configuration/01_Country.js
@@ -23,4 +23,31 @@
 //ds.Country.all();
 
 
+//ds.Country.all();
+
+var countries = ds.Country.all();
+
+if (countries.length === 0) {
+	// ##### CREATE DEFAULT COUNTRIES #####
+
+var newCountry = new ds.Country();
+newCountry.name = "Austria";
+newCountry.iso = "at";
+newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
+newCountry.save();
+
+var newCountry = new ds.Country();
+newCountry.name = "United Kingdom";
+newCountry.iso = "de";
+newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
+newCountry.save();
+
+
+// ## SHOW ALL COUNTRIES ##
+
+
+
+}
+
+
 ds.Country.all();
