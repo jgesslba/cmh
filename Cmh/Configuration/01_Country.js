d4d503ad140c03f562bcdf05609579273b5f3b77
jgesslba <joerg.gesslbauer@gmail.com>
2014-06-25 20:00:53 +0200

    bootstraps
    Add systemdata.js file.

--- Cmh/Configuration/01_Country.js
+++ Cmh/Configuration/01_Country.js
@@ -1,24 +1,26 @@
-﻿// ##### DELETE ALL COUNTRIES #####
+//// ##### DELETE ALL COUNTRIES #####
 
-ds.Country.remove();
-
-
-// ##### CREATE DEFAULT COUNTRIES #####
-
-var newCountry = new ds.Country();
-newCountry.name = "Austria";
-newCountry.iso = "at";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
-newCountry.save();
-
-var newCountry = new ds.Country();
-newCountry.name = "United Kingdom";
-newCountry.iso = "de";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
-newCountry.save();
+//ds.Country.remove();
 
 
-// ## SHOW ALL COUNTRIES ##
+//// ##### CREATE DEFAULT COUNTRIES #####
+
+//var newCountry = new ds.Country();
+//newCountry.name = "Austria";
+//newCountry.iso = "at";
+//newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
+//newCountry.save();
+
+//var newCountry = new ds.Country();
+//newCountry.name = "United Kingdom";
+//newCountry.iso = "de";
+//newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
+//newCountry.save();
+
+
+//// ## SHOW ALL COUNTRIES ##
+
+//ds.Country.all();
+
 
 ds.Country.all();
-
