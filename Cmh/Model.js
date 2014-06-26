8ba5997b183c9d805deec59fd9faf9d1332bd3a9
jgesslba <joerg.gesslbauer@gmail.com>
2014-06-26 23:13:28 +0200

    Model
    Add getTopMenu function.

--- Cmh/Model.js
+++ Cmh/Model.js
@@ -2,3 +2,4 @@ include("./Model/User/User-events.js");
 include("./Model/Game/Game-events.js");
 include("./Model/Gamer/Gamer-events.js");
 include("./Model/Country/Country-events.js");
+include("./Model/Gamer/Gamer-methods.js");
---
+++ Cmh/Model/Gamer/Gamer-methods.js
@@ -0,0 +1,6 @@
+
+
+model.Gamer.entityMethods.getTopMenuArray = function() {
+	// Add your code here;
+};
+model.Gamer.entityMethods.getTopMenuArray.scope = 'public';
