463d29e8c6fad39b404fe15567fbef2921a1da2a
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-07 11:37:09 +0200

    Module
    Add menu.js module

--- Cmh/Modules/Clientside/Helper/menu.js
+++ Cmh/Modules/Clientside/Helper/menu.js
@@ -4,7 +4,21 @@
 	
 	For more information, refer to http://doc.wakanda.org/Wakanda0.Beta/help/Title/en/page1516.html
 */
-exports.helloWorld = function helloWorld () {
-	return ('Hello world');
+exports.buildTopBar = function buildTopBar () {
+	
+	// Get user permissions
+	var myUser = currentUser();
+	var userStorage = myUser.storage;
+	userStorage["language"] = userLanguage.substr(0,2);
+	return userStorage["language"];
+	
+	topNavData = [ // Array with menu item objects
+			{title: objTranslation.menuGamecenter, componentpath: "/components/gamecenter/gamecenter.waComponent", menucomponentpath: "/components/gamecenter/gamecenterMenu.waComponent", topNavItemImagePath: "/images/Medical_Office.png", topNavItemId: "navGamecenter", topNavItemClass: "topNavItemLi topNavItemSelected"},
+			{title: objTranslation.menuGamer, componentpath: "/components/gamer/gamerGame.waComponent", menucomponentpath: "/components/gamer/gamerMenu.waComponent", topNavItemImagePath: "/images/Medical_Office.png", topNavItemId: "navGamer", topNavItemClass: "topNavItemLi"},
+			{title: objTranslation.menuGamehost, componentpath: "/components/gamehost/gamehostHome.waComponent", menucomponentpath: "/components/gamehost/gamehostMenu.waComponent", topNavItemImagePath: "/images/User_Dentist.png", topNavItemId: "navGamehost", topNavItemClass: "topNavItemLi"},
+			{title: objTranslation.menuAdmin, componentpath: "/components/administrator/administratorHome.waComponent", menucomponentpath: "/components/administrator/adminMenu.waComponent", topNavItemImagePath: "/images/User_Dentist.png", topNavItemId: "navAdmin", topNavItemClass: "topNavItemLi"}
+		];
+		
+	return (topNavData);
 	
 };
