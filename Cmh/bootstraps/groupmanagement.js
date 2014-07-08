a247fe3ade485037763948c034cb13e2f6481e33
jgesslba <joerg.gesslbauer@gmail.com>
2014-07-08 07:40:31 +0200

    Groupmanagement
    Change permissions

--- Cmh/bootstraps/groupmanagement.js
+++ Cmh/bootstraps/groupmanagement.js
@@ -30,9 +30,35 @@ if (Game_Read === null) {
 }
 
 
+
 // ##### PUT THE ORGANIZATIONAL GROUPS INTO THE FUNCTIONAL GROUPS IN ORDER TO SET PERMISSIONS #####
+
+
+// USER CLASS
+
+Gameadmin.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
+Gameadmin.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
+Gameadmin.putInto(User_Update); // put the organizational Gameadmin group into the functional User_Update group
+Gameadmin.putInto(User_Remove); // put the organizational Gameadmin group into the functional User_Remove group
+Gameadmin.putInto(User_Describe); // put the organizational Gameadmin group into the functional User_Describe group
+Gameadmin.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
+Gameadmin.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
+
+Gamehost.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
+Gamehost.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
+Gamehost.putInto(User_Update); // put the organizational Gameadmin group into the functional User_Update group
+Gamehost.putInto(User_Remove); // put the organizational Gameadmin group into the functional User_Remove group
+Gamehost.putInto(User_Describe); // put the organizational Gameadmin group into the functional User_Describe group
+Gamehost.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
+Gamehost.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
+
+
+// GAME CLASS
 
 Gamehost.putInto(Game_Read); // put the organizational Gamehost group into the functional Game_Read group 
 Gameadmin.putInto(Game_Read); // put the organizational Gameadmin group into the functional Game_Read group
+
+
+
 
 // @endregion
