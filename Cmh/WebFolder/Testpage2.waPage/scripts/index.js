c90c0fa04cf375468f3d3bd213a7ce00cb17262f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-11 14:20:02 +0200

    Update #104

---
+++ Cmh/WebFolder/Testpage2.waPage/scripts/index.js
@@ -0,0 +1,26 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var bla = {};	// @htmlTag
+	var a = {};	// @htmlTag
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	bla.click = function bla_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		console.log("test");
+	};// @lock
+
+	a.click = function a_click (event)// @startlock
+	{// @endlock
+		alert("Hallo");
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("bla", "click", bla.click, "html");
+	WAF.addListener("a", "click", a.click, "html");
+// @endregion
+};// @endlock
