98b00c570553026c53e8b47fc667bd083e0acbd8
jgesslba <joerg.gesslbauer@gmail.com>
2014-09-15 18:06:14 +0200

    Change groupmanagement

--- Cmh/bootstraps/groupmanagement.js
+++ Cmh/bootstraps/groupmanagement.js
@@ -33,7 +33,54 @@ if (Gamer === null) {
 }
 	
 
+
 // ##### GET OR CREATE FUNCTIONAL GROUPS IN THE DIRECTORY #####
+
+
+// MODEL
+
+var Model_Read = directory.group("Model_Read"); // creates the group object
+if (Model_Read === null) {
+	var Model_Read = directory.addGroup("Model_Read", "Model_Read group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Create = directory.group("Model_Create"); // creates the group object
+if (Model_Create === null) {
+	var Model_Create = directory.addGroup("Model_Create", "Model_Create group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Update = directory.group("Model_Update"); // creates the group object
+if (Model_Update === null) {
+	var Model_Update = directory.addGroup("Model_Update", "Model_Update group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Remove = directory.group("Model_Remove"); // creates the group object
+if (Model_Remove === null) {
+	var Model_Remove = directory.addGroup("Model_Remove", "Model_Remove group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Describe = directory.group("Model_Describe"); // creates the group object
+if (Model_Describe === null) {
+	var Model_Describe = directory.addGroup("Model_Describe", "Model_Describe group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Execute = directory.group("Model_Execute"); // creates the group object
+if (Model_Execute === null) {
+	var Model_Execute = directory.addGroup("Model_Execute", "Model_Execute group");
+	directory.save(); // save the organizational groups in the directory
+}
+
+var Model_Promote = directory.group("Model_Promote"); // creates the group object
+if (Model_Promote === null) {
+	var Model_Promote = directory.addGroup("Model_Promote", "Model_Promote group");
+	directory.save(); // save the organizational groups in the directory
+}
+
 
 // USER CLASS
 
@@ -79,6 +126,9 @@ if (User_Promote === null) {
 	directory.save(); // save the organizational groups in the directory
 }
 
+
+// GAME CLASS
+
 var Game_Read = directory.group("Game_Read"); // creates the group object
 if (Game_Read === null) {
 	var Game_Read = directory.addGroup("Game_Read", "Game_Read group");
@@ -90,23 +140,34 @@ if (Game_Read === null) {
 // ##### PUT THE ORGANIZATIONAL GROUPS INTO THE FUNCTIONAL GROUPS IN ORDER TO SET PERMISSIONS #####
 
 
+// MODEL
+
+Admin.putInto(Model_Read); // put the organizational Admin group into the functional Model_Read group
+Admin.putInto(Model_Create); // put the organizational Admin group into the functional Model_Create group
+Admin.putInto(Model_Update); // put the organizational Admin group into the functional Model_Update group
+Admin.putInto(Model_Remove); // put the organizational Admin group into the functional Model_Remove group
+Admin.putInto(Model_Describe); // put the organizational Admin group into the functional Model_Describe group
+Admin.putInto(Model_Execute); // put the organizational Admin group into the functional Model_Execute group
+Admin.putInto(Model_Promote); // put the organizational Admin group into the functional Model_Promote group
+
+
 // USER CLASS
 
-Admin.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
-Admin.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
-Admin.putInto(User_Update); // put the organizational Gameadmin group into the functional User_Update group
-Admin.putInto(User_Remove); // put the organizational Gameadmin group into the functional User_Remove group
-Admin.putInto(User_Describe); // put the organizational Gameadmin group into the functional User_Describe group
-Admin.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
-Admin.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
+Admin.putInto(User_Read); // put the organizational Admin group into the functional User_Read group
+Admin.putInto(User_Create); // put the organizational Admin group into the functional User_Create group
+Admin.putInto(User_Update); // put the organizational Admin group into the functional User_Update group
+Admin.putInto(User_Remove); // put the organizational Admin group into the functional User_Remove group
+Admin.putInto(User_Describe); // put the organizational Admin group into the functional User_Describe group
+Admin.putInto(User_Execute); // put the organizational Admin group into the functional User_Execute group
+Admin.putInto(User_Promote); // put the organizational Admin group into the functional User_Promote group
 
-Internal.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
-Internal.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
-Internal.putInto(User_Update); // put the organizational Gameadmin group into the functional User_Update group
-Internal.putInto(User_Remove); // put the organizational Gameadmin group into the functional User_Remove group
-Internal.putInto(User_Describe); // put the organizational Gameadmin group into the functional User_Describe group
-Internal.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
-Internal.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
+Internal.putInto(User_Read); // put the organizational Internal group into the functional User_Read group
+Internal.putInto(User_Create); // put the organizational Internal group into the functional User_Create group
+Internal.putInto(User_Update); // put the organizational Internal group into the functional User_Update group
+Internal.putInto(User_Remove); // put the organizational Internal group into the functional User_Remove group
+Internal.putInto(User_Describe); // put the organizational Internal group into the functional User_Describe group
+Internal.putInto(User_Execute); // put the organizational Internal group into the functional User_Execute group
+Internal.putInto(User_Promote); // put the organizational Internal group into the functional User_Promote group
 
 Gameadmin.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
 Gameadmin.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
@@ -116,13 +177,13 @@ Gameadmin.putInto(User_Describe); // put the organizational Gameadmin group into
 Gameadmin.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
 Gameadmin.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
 
-Gamehost.putInto(User_Read); // put the organizational Gameadmin group into the functional User_Read group
-Gamehost.putInto(User_Create); // put the organizational Gameadmin group into the functional User_Create group
-Gamehost.putInto(User_Update); // put the organizational Gameadmin group into the functional User_Update group
-Gamehost.putInto(User_Remove); // put the organizational Gameadmin group into the functional User_Remove group
-Gamehost.putInto(User_Describe); // put the organizational Gameadmin group into the functional User_Describe group
-Gamehost.putInto(User_Execute); // put the organizational Gameadmin group into the functional User_Execute group
-Gamehost.putInto(User_Promote); // put the organizational Gameadmin group into the functional User_Promote group
+Gamehost.putInto(User_Read); // put the organizational Gamehost group into the functional User_Read group
+Gamehost.putInto(User_Create); // put the organizational Gamehost group into the functional User_Create group
+Gamehost.putInto(User_Update); // put the organizational Gamehost group into the functional User_Update group
+Gamehost.putInto(User_Remove); // put the organizational Gamehost group into the functional User_Remove group
+Gamehost.putInto(User_Describe); // put the organizational Gamehost group into the functional User_Describe group
+Gamehost.putInto(User_Execute); // put the organizational Gamehost group into the functional User_Execute group
+Gamehost.putInto(User_Promote); // put the organizational Gamehost group into the functional User_Promote group
 
 
 // GAME CLASS
