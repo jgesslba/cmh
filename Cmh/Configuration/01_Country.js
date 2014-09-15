e6943aa31f828642b7aa67ea91e56ef100612b57
jgesslba <joerg.gesslbauer@gmail.com>
2014-09-15 17:30:01 +0200

    Fix syntax error

--- Cmh/Configuration/01_Country.js
+++ Cmh/Configuration/01_Country.js
@@ -8,13 +8,13 @@ ds.Country.remove();
 var newCountry = new ds.Country();
 newCountry.name = "Austria";
 newCountry.iso = "at";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png" // Windows
+newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/at.png"; // Windows
 newCountry.save();
 
 var newCountry = new ds.Country();
 newCountry.name = "United Kingdom";
 newCountry.iso = "de";
-newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png" // Windows
+newCountry.flag = "C:/gesslbauer/Wakanda/cmh/cmh/WebFolder/images/countries/gb.png"; // Windows
 newCountry.save();
 
 
