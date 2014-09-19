2aa636d277c70183ab07c63367f387ad8463ebaf
jgesslba <joerg.gesslbauer@gmail.com>
2014-09-19 18:42:44 +0200

    Update #149

--- Cmh/WebFolder/components/administrator/administratorUserAdmin.waComponent/administratorUserAdmin.js
+++ Cmh/WebFolder/components/administrator/administratorUserAdmin.waComponent/administratorUserAdmin.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿
 (function Component (id) {// @lock
 
@@ -26,3 +27,33 @@ function constructor (id) {
 }// @startlock
 return constructor;
 })();// @endlock
+=======
+
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'administratorUserAdmin';
+	// @endregion// @endlock
+
+	this.load = function (data) {// @lock
+
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
+>>>>>>> Stashed changes
